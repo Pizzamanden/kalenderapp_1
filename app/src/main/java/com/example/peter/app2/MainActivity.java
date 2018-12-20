2ed7fe2839d508463b985d1367ea01cd35723851
Peter Winther <dialga399@live.dk>
2018-12-20 14:37:18 +0100

    OkHttp virker på egen phone

--- app/src/main/java/com/example/peter/app2/MainActivity.java
+++ app/src/main/java/com/example/peter/app2/MainActivity.java
@@ -6,8 +6,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mWeekNr = new ArrayList<>();
     private ArrayList<ArrayList<String>> mDates = new ArrayList<>();
     private int dateNumber;
+    private String myHttpString;
+    public TextView textView_users;
+    private String folderolURL = "http://www.folderol.dk/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         dateNumber = 1;
         Log.d(TAG, "onCreate: Created");
-
+        textView_users = findViewById(R.id.textView_users);
         initContentArrays();
     }
 
@@ -54,6 +65,52 @@ public class MainActivity extends AppCompatActivity {
             mDates.add(makeWeekDates());
         }
         initRecyclerView();
+    }
+
+    public void startHttpReq(View view)
+    {
+        String thisURL = folderolURL + "";
+        makeHttpReq(thisURL);
+        // Min Url
+    }
+    public void httpCallback(String response)
+    {
+        textView_users.setText(response);
+    }
+
+    public void makeHttpReq(String url)
+    {
+        Log.d(TAG, "getHttpReq: Fired OkHttp Method");
+        Log.d(TAG, "getHttpReq: Building Client");
+        OkHttpClient client = new OkHttpClient();
+        Log.d(TAG, "getHttpReq: Building Request");
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Log.d(TAG, "getHttpReq: Firing Call");
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG, "getHttpReq: Failure");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "getHttpReq: Response");
+                if(response.isSuccessful()){
+                    Log.d(TAG, "getHttpReq: Response successful");
+                    final String mResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            httpCallback(mResponse);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void initRecyclerView()
